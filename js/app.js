6c17eee11bf984677f9ca1f1b7d1ac75652dc9e0
Dillon <44330273+DillonNotDylan@users.noreply.github.com>
2021-05-24 18:05:39 -0400

    A folder to house the websites JavaScript files.
    The included folder is JS logic for using the API to search for contacts.

---
+++ js/app.js
@@ -0,0 +1,36 @@
+var urlBase = 'http://www.thebest-group28.xyz/LAMPAPI'
+var extension = 'php';
+
+var userId = 0;
+var firstName = "";
+var lastName = "";
+
+function searchContacts()
+{
+    var searcher = document.getElementById("searchContacts").values;
+    document.getElementById("contactSearchResults").innerHTML = ""
+
+    var contact = "";
+
+    var jsonPayLoad = '{"search" : "' + contact + '", "userId" : ' + userId + '}';
+    var url = urlBase + '/ContactSearch.' + extension;
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                document.getElementById()
+            }
+        }
+    }
+    catch(err)
+    {
+        document.getElementById("SearchResults").innerHTML = err.message;
+    }
+}
