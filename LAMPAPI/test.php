90deaa64ac3b3872ac58cb891b2a433dc930430d
Angel <angel_emmanuel_v@hotmail.com>
2021-05-27 13:40:47 -0400

    testing

--- LAMPAPI/test.php
+++ LAMPAPI/test.php
@@ -1,9 +1,19 @@
 <?php
-        $obj->name = "thing";
-        $obj->code = 12;
+        header('Access-Control-Allow-Origin: *');
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
-        $myresp  = json_encode($obj);
 
-        echo $myresp;
+        //$thing = json_decode(file_get_contents('php://input'), true);
+        class thing
+        {
+                var $name;
+                var $code;
+        }
+
+        $obj = new thing;
+
+        header('Content-type: application/json');
+        $obj = json_encode($obj);
+        echo '{"name": "bob"}';
 ?>
-~        
