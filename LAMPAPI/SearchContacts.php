97cffc1e17e0fb4e5d94e441b4e65c56171f050d
jp val <70183331+jp-val@users.noreply.github.com>
2021-05-27 14:07:13 -0400

    Update SearchContacts.php

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -1,62 +1,64 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
     
-    $inData = getRequestInfo();
-
-    $searchResults = "";
-
-    $searchCount = 0;
-
-    $conn = new mysql("local", "small", "password", "smallproject");
-
-    if($conn->connect_error)
-    {
-        returnWithError($conn->connect_error);
-    }
-    else
-    {
-        $stmt = $conn->prepare("SELECT * FROM Users WHERE FirstName = ? AND LastName = ?");
-		$stmt->bind_param("ss", $inData["first_name"], $inData["last_name"]); // The JSON params sent from Javascript
+	$inData = json_decode(file_get_contents('php://input'), true);
+	
+	$conn = new mysqli("localhost", "small", "password", "smallproject");
+	if ($conn->connect_error) 
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("SELECT * FROM ContactList WHERE UserID = ? AND FirstName LIKE ? AND LastName LIKE ?");
+		$firstName = "%" . $inData["first_name"] . "%";
+		$lastName = "%" . $inData["last_name"] . "%";
+		$stmt->bind_param("sss", $inData["id"], $firstName, $lastName);
 		$stmt->execute();
+		
 		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if($searchCount > 0)
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["FirstName"] . '"';
+		}
+		
+		if($searchCount == 0)
+		{
+			returnWithError("No Records Found");
+		}
+		else
+		{
+			returnWithInfo($searchResults);
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
 
-        if($searchCount == 0)
-        {
-            returnWithError("There are no contacts in your list that go by that name.");
-        }
-        else
-        {
-            returnWithInfo($searchResults);
-        }
-
-        $stmt->close();
-        $conn->close();
-    }
-
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
-
-    function sendResultInfoAsJson($obj)
-    {
-        header('Content-type: application/json');
-
-        echo $obj;
-    }
-
-    function returnWithError($err)
-    {
-        $retValue = '{"id":0, "firstName":"", "lastName":"", "error":"' . $err . '"}';
-        sendResultInfoAsJson($retValue);
-    }
-
-    function returnWithInfo($searchResults)
-    {
-        $retValue = '{"results":[' . $searchResults . '],"error":""}';
-        sendResultInfoAsJson($retValue);
-    }
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+	function returnWithInfo($searchResults)
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson($retValue);
+	}
 
 ?>
