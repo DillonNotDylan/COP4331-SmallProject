7657cc844081fb46fe7d73fd5d7c5bcb85499b90
Angel <angel_emmanuel_v@hotmail.com>
2021-05-27 11:52:56 -0400

    testing

---
+++ LAMPAPI/test.php
@@ -0,0 +1,9 @@
+<?php
+        $obj->name = "thing";
+        $obj->code = 12;
+
+        $myresp  = json_encode($obj);
+
+        echo $myresp;
+?>
+~        
--- js/register.js
+++ js/register.js
@@ -1,71 +1,23 @@
 // insert init's here
 var userN, passO, passT;
+let url = "http:/thebest-group28.xyz/LAMPAPI/test.php"
 
-
-function userExists(user)
-{
-	// create request with only user
-	var obj = {login: user};
-	var jsonPayload = JSON.stringify(obj);
-
-	var xhr = new XMLHttpRequest();
-	xhr.responseType = 'json';
-	let url = "LAMPAPI/test.php";
-
-	// start connection
-	xhr.open("POST", url, true);
-
-	// state change indicating recieved request
-	xhr.onreadystatechange = function()
-	{
-		// succesfully sent
-		if (this.readyState == 4 && this.status == 200)
-		{
-			var jsonObject = JSON.parse( xhr.responseText );
-
-			var lName = jsonObject.login;
-			document.getElementById("outp").innerHTML() = lName;
-		}
-		else
-		{
-			// error out: error code try again	
-			return 1;
-		}
-	};
-
-	// actually send JSON
-	xhr.send(jsonPayload);
-
-	return 0;
-}
 
 function addUser()
 {
-	var obj = {user: userN, pass: passW};
+	var obj = {user: userN, pass: passO};
 	var jsonPayload = JSON.stringify(obj);
 
 	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
 	xhr.responseType = 'json';
-	let url = 'http://www.thebest-group28.xyz/LAMPAPI';
 
 	xhr.onreadystatechange = function() 
 	{
 		if (this.readyState == 4 && this.status == 200) 
 		{
 			var jsonObject = JSON.parse( xhr.responseText );
-			userId = jsonObject.id;
-			if( userId < 1 )
-			{		
-				document.getElementById("outp").innerHTML = "User already exists";
-				return;
-			}
-		
-			firstName = jsonObject.firstName;
-			lastName = jsonObject.lastName;
-
-			saveCookie();
-			window.location.href = "contactsPage.html";
-				
+			document.getElementById("outp").innerHTML = JSON.stringify(jsonObject);	
 		}
 	};
 	xhr.send(jsonPayload);
@@ -88,6 +40,7 @@ function checkValid()
 	}
 
 	document.getElementById("outp").innerHTML = "";
+	addUser();
 
 	// add user to DB
 
