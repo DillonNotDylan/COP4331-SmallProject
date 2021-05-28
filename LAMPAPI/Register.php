f289e6e33f5344aaa2138a20f272eab9ee1a3054
LuisBiceps <54749472+LuisBiceps@users.noreply.github.com>
2021-05-27 20:59:12 -0400

    Implemented Register.php with hashing

---
+++ LAMPAPI/Register.php
@@ -0,0 +1,37 @@
+<?php
+
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+	$inData = json_decode(file_get_contents('php://input'), true);
+
+	$conn = new mysqli("localhost", "small", "password", "smallproject");
+	
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Username, Password) VALUES (?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $inData["first"], $inData["last"], $inData["user"], $inData["pass"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+?>
