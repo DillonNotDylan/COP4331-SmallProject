7861a1e0266d7145e8589a6702a45110bdd1a15e
Luis Reyes Rosado <reyesrosadoluis1@gmail.com>
2021-05-27 22:18:38 -0400

    Goes to home page with successful register

--- js/register.js
+++ js/register.js
@@ -33,6 +33,11 @@ function addUser()
 				if(jsonObject.error[0] == "")
 				{
 					alert("User registered");
+					userId = 0;
+					firstName = "";
+					lastName = "";
+					document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+					window.location.href = "index.html";
 				}
 			}
 		};
