5f713a4eeb9408fe7c7ade8e23a19379a021b76f
LuisBiceps <54749472+LuisBiceps@users.noreply.github.com>
2021-05-27 20:58:36 -0400

    Redid register.js with hashing

--- js/register.js
+++ js/register.js
@@ -1,28 +1,48 @@
 // insert init's here
-var userN, passO, passT;
-let url = "http:/www.thebest-group28.xyz/LAMPAPI/test.php"
+var firstN, lastN, userN, passO, passT;
+var urlBase = "http:/www.thebest-group28.xyz/LAMPAPI/test.php"
 
 
 function addUser()
 {
-	var obj = {user: userN, pass: passO};
-	var jsonPayload = JSON.stringify(obj);
+	// Capture the text from the search bars to use for querying
+	var first = document.getElementById("first").value;
+	var last = document.getElementById("last").value;
+	var user = document.getElementById("user").value;
+	var pass = document.getElementById("pass").value;
+	var hash = md5(pass);
+	var repass = document.getElementById("repass").value;
+	var rehash = md5(repass);
 
+	// Prepare variables for the API
+	var jsonPayload = '{"first" : "' + first + '", "last" : "' + last + '", "user" : "' + user + '", "pass" : "' + pass +'"}';
+	console.log("jsonPayLoad: " + jsonPayload);
+	var url = urlBase + '/Register.' + extension;
+
+	// Attempt a connection to the API
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
-	xhr.responseType = 'json';
-
-	xhr.onreadystatechange = function() 
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
 	{
-		if (this.readyState == 4 && this.status == 200) 
+		xhr.onreadystatechange = function()
 		{
-			var jsonObject = JSON.parse( xhr.responseText );
-			document.getElementById("outp").innerHTML = "finished";	
-		}
-	};
-
-	xhr.send(jsonPayload);
-
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				var jsonObject = JSON.parse( xhr.responseText ); // Response from the API
+			
+				if(jsonObject.error[0] == "")
+				{
+					alert("User registered");
+				}
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
+	}
 }
 
 function checkValid()
@@ -30,11 +50,11 @@ function checkValid()
 
 	userN = document.getElementById("user").value;
 	passO = document.getElementById("pass").value;
+	var hashO = md5(passO);
 	passT = document.getElementById("repass").value;
+	var hashT = md5(passT);
 
-	
-
-	if (passO != passT)
+	if (hashO != hashT)
 	{
 		document.getElementById("outp").innerHTML = "Warning: passwords don't match!";
 		return;
@@ -42,5 +62,4 @@ function checkValid()
 
 	document.getElementById("outp").innerHTML = "";
 	addUser();
-
 }
