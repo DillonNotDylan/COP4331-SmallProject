0e1d6ad68d56a7088ab7cbb143d913ef04f73350
Luis Reyes Rosado <reyesrosadoluis1@gmail.com>
2021-06-02 14:34:12 -0400

    Added DeleteConact php file

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,37 @@
+<?php
+
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+	$inData = json_decode(file_get_contents('php://input'), true);
+
+	$conn = new mysqli("localhost", "small", "password", "smallproject");
+	
+	if ($conn->connect_error)
+	{
+		returnWithError($conn->connect_error);
+	}
+	else
+	{
+		$stmt = $conn->prepare("DELETE FROM ContactList WHERE ContactID = 'contactId'");
+		$stmt->bind_param("s", $inData["contactId"]);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function sendResultInfoAsJson($obj)
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError($err)
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson($retValue);
+	}
+	
+?>
