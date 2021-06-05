0c0cc44b6050e1934e8de6a0029c225d005a2b25
Dillon <44330273+DillonNotDylan@users.noreply.github.com>
2021-06-05 09:55:16 -0400

    typo

--- js/contactsPage.js
+++ js/contactsPage.js
@@ -204,7 +204,7 @@ function edit_contact()
 	var first_name = document.getElementById("edit_first_name").innerHTML;
 	var last_name = document.getElementById("edit_last_name").innerHTML;
 	var phone_number = document.getElementById("edit_phone_number").innerHTML;
-	var email = document.getElementById("edit_address").innerHTML;
+	var email = document.getElementById("edit_email").innerHTML;
 
 	// Prepare variables for the API
 	var jsonPayload = '{"userId" : "' + userId + '", "contactId" : "' + contactId + '", "first_name" : "' + first_name + '", "last_name" : "' + last_name + '", "phone_number" : "' + phone_number + '", "email" : "' + email + '"}';
